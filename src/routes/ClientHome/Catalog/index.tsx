0204a696f18cbf64e2e571708d2a9bf8880781d7
elissandroa <elissandroa@hotmail.com>
2023-12-18 12:25:33 -0300

    Catalog next page

--- src/routes/ClientHome/Catalog/index.tsx
+++ src/routes/ClientHome/Catalog/index.tsx
@@ -5,23 +5,42 @@ import ButtonNextPage from '../../../components/ButtonNextPage';
 import ProductCard from '../../../components/ProductCard';
 import { useState, useEffect } from 'react';
 import { ProductDTO } from '../../../models/product';
+
+type Queryparams = {
+  page: number;
+  name: string;
+}
+
 export default function Catalog() {
 
+  const [queryParams, setQueryParams] = useState<Queryparams>({
+    page: 0,
+    name: ""
+  });
+  const [isLastPage, setIsLastPage] = useState(false);
+
   const [products, setProducts] = useState<ProductDTO[]>([]);
-  const [productName, setProductName] = useState("");
+
 
 
 
   useEffect(() => {
-    productService.findPageRequest(0, productName)
+    productService.findPageRequest(queryParams.page, queryParams.name)
       .then(response => {
-        setProducts(response.data.content);
+        const nextPage = response.data.content;
+        setProducts(products.concat(nextPage));
+        setIsLastPage(response.data.last);
       });
 
-  }, [productName])
+  }, [queryParams])
 
-  function handleSearch(searchText:string) {
-    setProductName(searchText);
+  function handleSearch(searchText: string) {
+    setProducts([]);
+    setQueryParams({ ...queryParams, page: 0, name: searchText });
+  }
+
+  function handleNextPageClick() {
+    setQueryParams({ ...queryParams, page: queryParams.page + 1 })
   }
 
   return (
@@ -37,7 +56,12 @@ export default function Catalog() {
               ))
             }
           </div>
-          <ButtonNextPage />
+          {!isLastPage &&
+            <div onClick={handleNextPageClick}>
+              <ButtonNextPage />
+            </div>
+          }
+
         </section>
       </main>
     </>
