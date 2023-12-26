a6076efb88887c1ebcc355ebb04998890bce613a
elissandroa <elissandroa@hotmail.com>
2023-12-26 19:12:08 -0300

    RequestBackend update withCredentials

--- src/services/user-service.ts
+++ src/services/user-service.ts
@@ -1,10 +1,12 @@
+import { AxiosRequestConfig } from "axios";
 import { requestBackend } from "../utils/requests";
-import * as authService from './auth-service';
-
-const headers = {
-    Authorization: "Bearer " +  authService.getAccessToken()
-}
 
 export function findMe() {
-    return requestBackend({url: '/users/me', headers});
+
+    const config: AxiosRequestConfig = {
+        url: "users/me",
+        withCredentials: true
+    }
+
+    return requestBackend(config);
 }
