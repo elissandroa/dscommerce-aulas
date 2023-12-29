c1ca5c3775e78e28e6cf6a1bf9830f1bc46f80dd
elissandroa <elissandroa@hotmail.com>
2023-12-29 15:32:36 -0300

    Confirmation

--- src/App.tsx
+++ src/App.tsx
@@ -15,6 +15,7 @@ import { AccessTokenPayloadDTO } from "./models/auth";
 import { ContextToken } from "./utils/context-token";
 import * as authService from './services/auth-service';
 import * as cartService from './services/cart-service';
+import Confirmation from "./routes/ClientHome/Confirmation";
 
 export default function App() {
   const [contextCartCount, setContextCartCount] = useState<number>(0);
@@ -39,6 +40,7 @@ export default function App() {
               <Route path='/product-details/:productId' element={<ProductDetails />} />
               <Route path="/cart" element={<Cart />} />
               <Route path="/login" element={<Login />} />
+              <Route path="/confirmation/:orderId" element={<Confirmation />} />
             </Route>
             <Route path="/admin/" element={<PrivateRoute roles={['ROLE_ADMIN']}><Admin /></PrivateRoute>}>
               <Route index element={<AdminHome />} />
