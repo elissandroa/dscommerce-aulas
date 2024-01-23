6ff75dd24c42156d572e0e38f28560365d86f74e
elissandroa <elissandroa@hotmail.com>
2024-01-23 11:33:11 -0300

    FormTextArea

--- src/routes/Admin/ProductForm/index.tsx
+++ src/routes/Admin/ProductForm/index.tsx
@@ -5,6 +5,7 @@ import { useEffect, useState } from 'react';
 import * as forms from '../../../utils/forms';
 import FormInput from '../../../components/FormInput';
 import * as productService from '../../../services/product-service';
+import FormTextArea from '../../../components/FormTextArea';
 
 export default function ProductForm() {
 
@@ -42,6 +43,17 @@ export default function ProductForm() {
             name: "imgUrl",
             type: "text",
             placeholder: "Imagem",
+        },
+        description : {
+            value: "",
+            id: "description",
+            name: "description",
+            type: "text",
+            placeholder: "Descrição",
+            validation: function(value: string) {
+                return /^.{10,}/.test(value)
+            },
+            message: "A descrição deve ter pelo menos 10 caracteres"
         }
 
     })
@@ -99,8 +111,18 @@ export default function ProductForm() {
                                 />
                                 <div className='dsc-form-error'>{formData.imgUrl.message}</div>
                             </div>
+                            <div>
+                                <FormTextArea
+                                    {...formData.description}
+                                    className="dsc-form-control dsc-textarea"
+                                    onTurnDirty={handleTurnDirty}
+                                    onChange={handleInputChange}
+                                />
+                                <div className='dsc-form-error'>{formData.description.message}</div>
+                            </div>
                         </div>
-
+                        
+                        
                         <div className="dsc-product-form-buttons">
                             <Link to={"/admin/products"}>
                                 <button type="reset" className="dsc-btn dsc-btn-white">Cancelar</button>
