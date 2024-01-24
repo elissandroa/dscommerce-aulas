d4853aab3ca6de593b2364f74643dc2498c78ec8
elissandroa <elissandroa@hotmail.com>
2024-01-24 20:11:26 -0300

    Backend errors

--- src/utils/forms.ts
+++ src/utils/forms.ts
@@ -80,3 +80,13 @@ export function hasAnyInvalid(inputs: any) {
   }
   return false;
 }
+
+export function setBackendErrors(inputs: any, erros: any[]) {
+  const newInputs = { ...inputs};
+  erros.forEach(item => {
+    newInputs[item.fieldName].message = item.message;
+    newInputs[item.fieldName].dirty = "true";
+    newInputs[item.fieldName].invalid = "true";
+  });
+  return newInputs;
+}
