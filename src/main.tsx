b2251ac13a87baae56ecbbf0548eb9e597a428da
elissandroa <elissandroa@hotmail.com>
2024-01-25 13:13:39 -0300

    Deploy ready

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,9 @@
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import React from 'react';
 
+window.React = React;
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 
